37ae96a3ab586856d08ebb450ce6d7e22a4f845b
Jake Ashton <jakemaddenashton@gmail.com>
2022-09-06 21:55:03 -0700

    Create dishes and disclaimer for menu page

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,6 +1,28 @@
 export default function displayMenu() {
     const content = document.getElementById('content');
 
+    class Dish {
+        constructor(name, price) {
+            this.name = name;
+            this.price = price;
+            this.ingredients = '';
+        }
+    }
+
+    const bolognaSandwich = new Dish('Bologna Sandwich', '$49');
+    bolognaSandwich.ingredients = `Oscar Meyer bologna, miracle whip, stale wonder bread, cut in half, *`;
+
+    const cornDawgs = new Dish('Corn Dawgs', '$40');
+    cornDawgs.ingredients = 'corn, dawgs, side of bbq sauce, a bistro favorite!, *';
+
+    const famousChili = new Dish('Famous Chili, $100');
+    famousChili.ingredients = 'baked beans, chicken broth, dissolved Kraft singles, choice of meat, *';
+
+    const brendanBuffet = new Dish("Brendan's Buffet", '$500');
+    brendanBuffet.ingredients = 'Watch George of the Jungle himself eat his choice of bistro dishes, all Brendan can eat, leftovers up for grabs';
+
+    const disclaimer = '* = For Bren Dog only. Please note customers are still responsible for the dish price.'
+
     // Create page elements
     const header = document.createElement('h1');
     const brendanEating = document.createElement('img');
